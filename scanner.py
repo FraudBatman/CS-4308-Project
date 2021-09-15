197c157fc98305364980cd7a2cc3b45485a1c9de
Nic Ott <nicsonfire4@gmail.com>
2021-09-15 14:26:21 -0400

    basic file IO and a couple of IO test files

--- scanner.py
+++ scanner.py
@@ -1,5 +1,14 @@
-# comments?
-# comments!
+# to run this in a terminal, use the command "python -i scanner.py" in this directory
+
+# where filePath is a function parameter that's just a file name
 def scanner(filePath):
-    print("lol poop")
-    return filePath
+
+    # open file located at filePath, assign to variable file
+    file = open(filePath)
+
+    # for each syntax for every line in the file
+    for line in file:
+        print(line)  # print the line. duh.
+
+    # gets the name of the file and ends the function
+    return file.name
