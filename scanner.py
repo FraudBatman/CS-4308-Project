55d03ba19c99c87c2bd2938879aed6d2ca425f13
Sam Perez <srperez0619@yahoo.com>
2021-09-15 14:31:13 -0400

    working on comments

--- scanner.py
+++ scanner.py
@@ -3,3 +3,15 @@
 def scanner(filePath):
     print("lol poop")
     return filePath
+
+
+# Comment :) 
+#
+# To print, we want the output to be 
+# Keywords: kw1, kw2, kw3, kw4, kww5, kw6...
+# Identifiers: id1, id2, id3, id4, id5, id6....
+#
+# To do so, the file needs to be scanned line by line, each word read, and the keywords/identifiers 
+# need to be added to an array(potentially of strings) into an array. 
+#
+# To print 
