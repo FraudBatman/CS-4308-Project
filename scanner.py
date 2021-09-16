964ec5d2584fe071e25318e2ae0f939e5cec34d8
Jason Paek <adventuredonut343@gmail.com>
2021-09-16 16:19:57 -0400

    progress with lists

--- scanner.py
+++ scanner.py
@@ -1,5 +1,12 @@
 # to run this in a terminal, use the command "python -i scanner.py" in this directory
+# (Jason Keep) keywordList = {"function": "def", "define": "="}
+Keywords = ["function", "define"]
+Operators = []
+VariableNames = []
 
+keyWordsFound = []
+operatorsFound = []
+variableNames = []
 # where filePath is a function parameter that's just a file name
 def scanner(filePath):
 
@@ -9,6 +16,13 @@ def scanner(filePath):
     # for each syntax for every line in the file
     for line in file:
         print(line)  # print the line. duh.
+    for keywordItem in Keywords:
+        for 
+     print (keywordItem)
 
     # gets the name of the file and ends the function
     return file.name
+
+
+
+scanner("test.scl")
