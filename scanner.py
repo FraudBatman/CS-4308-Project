cefb98595084da37d16b6a82f8cbda6bca68e9d0
Nicholas Ott <nicsonfire4@gmail.com>
2021-09-17 09:42:46 -0400

    now gets each word individually

--- scanner.py
+++ scanner.py
@@ -8,31 +8,33 @@ keyWordsFound = []
 operatorsFound = []
 variableNames = []
 # where filePath is a function parameter that's just a file name
+
+
 def scanner(filePath):
-# Comment :) 
-#
-# To print, we want the output to be 
-# Keywords: kw1, kw2, kw3, kw4, kww5, kw6...
-# Identifiers: id1, id2, id3, id4, id5, id6....
-#
-# To do so, the file needs to be scanned line by line, each word read, and the keywords/identifiers 
-# need to be added to an array(potentially of strings) into an array. 
-#
-# To print 
+    # Comment :)
+    #
+    # To print, we want the output to be
+    # Keywords: kw1, kw2, kw3, kw4, kww5, kw6...
+    # Identifiers: id1, id2, id3, id4, id5, id6....
+    #
+    # To do so, the file needs to be scanned line by line, each word read, and the keywords/identifiers
+    # need to be added to an array(potentially of strings) into an array.
+    #
+    # To print
 
     # open file located at filePath, assign to variable file
     file = open(filePath)
 
     # for each syntax for every line in the file
     for line in file:
-        print(line)  # print the line. duh.
-    for keywordItem in Keywords:
-        for 
-     print (keywordItem)
+        # splits lines into individual words
+        lineList = line.split()
+        for word in lineList:
+            # word.strip() gives us the word without whitespace, we can use this to compare against keywords, operators, variables
+            print(word.strip())
 
     # gets the name of the file and ends the function
     return file.name
-
 
 
 scanner("test.scl")
