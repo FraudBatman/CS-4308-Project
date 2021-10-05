f346e039b6e0eee7705b290ba0f981c74712394a
Jason Paek <adventuredonut343@gmail.com>
2021-10-05 12:21:41 -0400

    Initiated the parser program and added some changes

---
+++ Supplemental Files/Mod 3 Project Deliverables/parser.py
@@ -0,0 +1,7 @@
+# CS 4308
+# Group Members: Nic Ott, Jason Paek, Sam Perez
+# Parser program to our scanner
+# The Parser takes input data (frequently text) 
+# and builds a data structure (parse tree, abstract syntax tree or other hierarchical structure)
+# Giving a structural representation of the input while checking for correct syntax.
+
