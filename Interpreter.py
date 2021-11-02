a674cf25f2bec59df59a6ca7a085f07d6a516d57
Jason Paek <adventuredonut343@gmail.com>
2021-11-02 17:42:18 -0400

    import

--- Interpreter.py
+++ Interpreter.py
@@ -3,3 +3,6 @@
 # This is our Interpreter program and it translates the tokens found by the scanner and organized by the parser
 # In other words it converts the tokens into executable instructions in the IDE
 
+from scanner import *
+from parser import *
+
